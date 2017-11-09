d3be61a67d45fe72d564d36099f78f7fc7103bf9
mtellin <mtellin@mtsgmbp.local>
2017-11-09 10:32:02 -0600

    Updated

--- PowerCLI One Liners.ps1
+++ PowerCLI One Liners.ps1
@@ -1,2 +1,10 @@
 # List all dvSwitch Port Groups that have Netflow enabled, formatted to only show name and enabled status (which should be all true)
+# Used to validate after configuring vRealize Network Insight
 Get-VDPortgroup | where {$_.ExtensionData.Config.defaultPortconfig.ipfixenabled.value -eq "True"} | Select Name, @{Name="NetflowEnabled";Expression={$_.Extensiondata.Config.defaultPortConfig.ipfixEnabled.Value}}
+
+# This will return the list of NSX Security Groups that are backed by Active Directory Groups for you to review
+Get-NsxSecurityGroup | Where {$_.InnerXml -like "*DirectoryGroup*"}
+
+# Alternatively, to also remove the security groups as part of the command 
+# Note: you will be prompted to confirm the group(s) it finds
+Get-NsxSecurityGroup | Where {$_.InnerXml -like "*DirectoryGroup*"} | Remove-NsxSecurityGroup
